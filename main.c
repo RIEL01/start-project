df0115835106da517de3c3b699802744794923a1
Lukas <you@example.com>
2018-07-10 15:47:25 +0200

    main.c Blinklicht verändert

--- main.c
+++ main.c
@@ -6,19 +6,14 @@
 int main(void)
 {
 DDRB=0xFF;      //Ausgang oder Einagang//
-PORTB=0b01010101;    //LED ein oder aus//
+PORTB=0xFF;    //LED ein oder aus//
 
-uint8_t var=0;
-uint16_t x=0;
 
-DDRC=0b11000000
+
 
 	
 	while(1)
 	{ 
-		var++;
-		PORTB=var;
-		for(x=0; x<60000; x++);
 		
 	} //end while
 }//end of main
